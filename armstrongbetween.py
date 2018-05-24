b6262732bfee11ef2db0d838a3be769c54411cd2
smadhumitha <39478133+smadhumitha@users.noreply.github.com>
2018-05-24 15:14:50 +0530

    armstrong

---
+++ armstrongbetween.py
@@ -0,0 +1,11 @@
+n1=int(input("enter a number"))
+n2=int(input("enter a number"))
+for i in range(n1,n2):
+    sum=0
+    num=n
+while(n>0):
+     digit=n%10
+     sum=sum+(r**3)
+     n=n/10
+     if(sum=num):
+       print(n)

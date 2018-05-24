cbd39815b1c3d72b254788d8f8c43ac03a88c3d5
smadhumitha <39478133+smadhumitha@users.noreply.github.com>
2018-05-24 15:46:48 +0530

    matrix

---
+++ matrix.py
@@ -0,0 +1,8 @@
+m1=[1,2,3],[1,2,3],[1,2,3]
+m2=[1,2,3],[1,2,3],[1,2,3]
+m=[[0,0,0],[0,0,0],[0,0,0]
+for i in range(len(m1,m2)):
+  for j in range(len(m2)):
+   m[i][j]=m1[i][j]+m2[i][j]
+   print(m[i][j],sep="\t",end=")
+         print()

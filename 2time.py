4c5237904c6de6badfc4e287ffa7ba3fd493f9b1
smadhumitha <39478133+smadhumitha@users.noreply.github.com>
2018-05-25 11:26:37 +0530

    2time

---
+++ 2time.py
@@ -0,0 +1,11 @@
+# include<stdio.h>
+void main()
+{
+int min1,hr1,min2,hr2;
+print("enter a two times in min&hr");
+scanf("%d%d",min1,hr1)
+scanf("%d%d",min2,hr2)
+hr=hr2-hr1
+min=min2-min1
+print("%d\t%d",min,hr);
+}

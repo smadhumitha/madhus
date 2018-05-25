8b898b89a7c09782357ed61d90292360a3061655
smadhumitha <39478133+smadhumitha@users.noreply.github.com>
2018-05-25 14:37:49 +0530

    large

--- large.py
+++ large.py
@@ -3,9 +3,9 @@ int main(void)
 {
 	int b[10],i,n,j,temp;
 	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	for(i=1;i<=n;++i)
 	scanf("%d",&b[i]);
-	for(j=i+1;j<=n;j++)
+	for(j=i+1;j<=n;++j)
 {
 	if(b[i]>b[j])
 {

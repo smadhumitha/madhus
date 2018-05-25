856199bb6dfa7cae8124db4e5e409442e8311155
smadhumitha <39478133+smadhumitha@users.noreply.github.com>
2018-05-25 11:55:27 +0530

    count

---
+++ count str.py
@@ -0,0 +1,9 @@
+string=input("enter a string:")
+vowels=0
+for i in string:
+   if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
+      vowels=vowels+1
+   else:
+       print("number of string:")
+       print(vowels)
+
